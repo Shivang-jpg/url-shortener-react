07ed80011080f582fa0e00cf7f5871f960ee9a70
Shivang-jpg <workforshivang07@gmail.com>
2026-09-15 15:47:19 +0530

    added the navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/NavBar'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
+
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/about' element={<AboutPage />} />
-
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   )
 }
